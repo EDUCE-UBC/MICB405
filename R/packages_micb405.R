26a82e20e7d206c7e8e2104b5771569622309a13
Stephan Koenig <stephan.koenig@gmail.com>
2021-10-21 13:08:03 -0700

    Fix list formatting & refactor package information

---
+++ R/packages_micb405.R
@@ -0,0 +1,5 @@
+if (!require(tidyverse))    install.packages("tidyverse")
+if (!require(pheatmap))     install.packages("pheatmap")
+if (!require(RColorBrewer)) install.packages("RColorBrewer")
+if (!require(BiocManager))  install.packages("BiocManager")
+if (!require(DESeq2))       BiocManager::install("DESeq2")
