845c08cce91de500b518a647d09e576200c20587
Stephan Koenig <stephan.koenig@gmail.com>
2021-10-28 08:50:38 -0700

    Add pasilla data set

---
+++ R/create_pasilla_gene_counts.R
@@ -0,0 +1,25 @@
+library(pasilla)
+library(tidyverse)
+
+counts_file <- system.file("extdata",
+                           "pasilla_gene_counts.tsv",
+                           package = "pasilla",
+                           mustWork = TRUE)
+metadata_file <- system.file("extdata",
+                             "pasilla_sample_annotation.csv",
+                             package = "pasilla",
+                             mustWork = TRUE)
+
+metadata <- read_csv(metadata_file)
+metadata <- select(metadata, file, condition, type)
+metadata <- mutate(metadata,
+                   condition = factor(condition),
+                   type = factor(type),
+                   file = str_remove(file, "fb")) %>% 
+  column_to_rownames("file")
+
+# Rearrange matrix columns in counts so they match the metadata
+counts <- read_tsv(counts_file) %>%
+  select(gene_id, rownames(metadata)) %>% 
+  mutate(gene_id = str_replace(gene_id, "FBgn", ""))
+write_csv(counts, here::here("data", "pasilla_gene_counts.csv"))
