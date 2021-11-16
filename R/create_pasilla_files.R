5a471240bfbef5a0a68b034511be8e265f3690c9
Stephan Koenig <stephan.koenig@gmail.com>
2021-11-16 11:41:49 -0800

    Update pasilla data set

--- R/create_pasilla_files.R
+++ R/create_pasilla_files.R
@@ -1,25 +1,26 @@
 library(pasilla)
 library(tidyverse)
 
-counts_file <- system.file("extdata",
-                           "pasilla_gene_counts.tsv",
-                           package = "pasilla",
-                           mustWork = TRUE)
+counts_file   <- system.file("extdata",
+                             "pasilla_gene_counts.tsv",
+                             package = "pasilla",
+                             mustWork = TRUE)
 metadata_file <- system.file("extdata",
                              "pasilla_sample_annotation.csv",
                              package = "pasilla",
                              mustWork = TRUE)
 
-metadata <- read_csv(metadata_file)
-metadata <- select(metadata, file, condition, type)
-metadata <- mutate(metadata,
-                   condition = factor(condition),
-                   type = factor(type),
-                   file = str_remove(file, "fb"))
+metadata <-
+  read_csv(metadata_file) %>% 
+  select(file, condition, type) %>% 
+  mutate(file = str_remove(file, "fb"))
 
-# Rearrange matrix columns in counts so they match the metadata
-counts <- read_tsv(counts_file) %>%
-  select(gene_id, rownames(metadata)) %>% 
+counts <-
+  read_tsv(counts_file) %>%
+  # Rearrange columns in `counts` so they match `metadata`
+  select(gene_id, metadata$file) %>% 
   mutate(gene_id = str_replace(gene_id, "FBgn", ""))
+
+# Save data frames
 write_csv(counts,   here::here("data", "pasilla_gene_counts.csv"))
 write_csv(metadata, here::here("data", "pasilla_metadata.csv"))
